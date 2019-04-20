c09f1d7f29e70543a1e591df99e6aaa5676158ee
Wesley Chiang <westhechiang@gmail.com>
2019-04-20 15:22:00 -0700

    Initial working scraper

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -9,22 +12,47 @@ import (
 
 func main() {
 	// Instantiate default collector
-	c := colly.NewCollector(
-		// Restricting domains to visit
-		colly.AllowedDomains("cannabis.lacity.org"),
-	)
+	c := colly.NewCollector()
 	c.IgnoreRobotsTxt = false
 	c.Limit(&colly.LimitRule{
-		DomainGlob: "https://cannabis.lacity.org/resources/authorized-retail-businesses/*",
-		Delay:      2 * time.Second,
+		// Set a delay between requests
+		Delay: 2 * time.Second,
+		// Set additional random delay
+		RandomDelay: 1 * time.Second,
 	})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+	detailCollector := c.Clone()
+
+	c.OnHTML("iframe", func(e *colly.HTMLElement) {
+		iframeURL := e.Attr("src")
 		// Print link
-		fmt.Printf("Link found: %q -> %s/n", e.Text, link)
+		// fmt.Printf("Link found: %q -> %s/n", e.Text, link)
 		//  Visit link found on page
-		c.Visit(e.Request.AbsoluteURL(link))
+		if strings.Contains(iframeURL, "google") {
+			fmt.Println("1st iframe", iframeURL)
+			parsedIframeURL, err := url.Parse(iframeURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			parsedIframeURL.RawQuery = ""
+			modifiedURL := parsedIframeURL.String()
+
+			detailCollector.Visit(modifiedURL)
+		}
+	})
+
+	detailCollector.OnHTML("tr", func(r *colly.HTMLElement) {
+		fmt.Println("2nd iframe")
+		fmt.Println(r.Text)
+	})
+
+	// c.OnResponse(func(r *colly.Response) {
+	// 	fmt.Println(r.Body)
+	// })
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
 	})
 
 	c.Visit("https://cannabis.lacity.org/resources/authorized-retail-businesses")
